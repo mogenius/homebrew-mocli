55565affd9eeeb3fac0f3c4b635eff2b32408808
Can Sürmeli <can@mogenius.com>
2023-08-14 10:08:58 +0200

    SHASUM change.

--- mocli.rb
+++ mocli.rb
@@ -2,7 +2,7 @@ class Mocli < Formula
   desc "View your mogenius account in style from your CLI environment!"
   homepage "https://www.mogenius.com"
   url "https://github.com/mogenius/homebrew-mocli/tree/main/releases/download/v1.0.0/mocli-1.0.0-darwin-arm64"
-  sha256 "950dce2b0a5180a8683de2a1c149bad8627657f01282c632ab5cb845aa0fc236"
+  sha256 "0aaeacfc45a9183dee2f317a5674b866aacf2e206824f2dee7d717e2d99b3f46"
   license "MIT License"
   version "1.0.0"
 
