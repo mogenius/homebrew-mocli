9bc4391a3682d7f9f2af900500bb96e9f8f83c7e
Can Sürmeli <can@mogenius.com>
2023-08-14 15:14:18 +0200

    New build.

--- mocli.rb
+++ mocli.rb
@@ -3,7 +3,7 @@ class Mocli < Formula
   homepage "https://www.mogenius.com"
   url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.0.0/mocli-1.0.0-darwin-arm64.tar.gz"
   version "1.0.0"
-  sha256 "552d0d4a6ed25bffc26d19793d90d2945d6942e5c815cb86f9eaa922899ca77d"
+  sha256 "69f5914386065c1252dbb241085c604ee288c5b7688be8fbb45915741c938ada"
   license "MIT"
 
   def install
