8176ab0299aed9ab15a3fc1b7d7a3456548811fc
Can Sürmeli <can@mogenius.com>
2023-08-14 09:40:40 +0200

    Minor fix.

--- mocli.rb
+++ mocli.rb
@@ -4,6 +4,7 @@ class Mocli < Formula
   url "https://github.com/mogenius/homebrew-mocli"
   sha256 "950dce2b0a5180a8683de2a1c149bad8627657f01282c632ab5cb845aa0fc236"
   license "MIT License"
+  version "1.0.0"
 
   def install
     bin.install "mocli"
