bc2f1283eeafb6f499dec27047aa0df08c74c302
Can Sürmeli <can@mogenius.com>
2023-08-14 10:01:08 +0200

    Minor fix.

--- mocli.rb
+++ mocli.rb
@@ -1,7 +1,7 @@
 class Mocli < Formula
   desc "View your mogenius account in style from your CLI environment!"
   homepage "https://www.mogenius.com"
-  url "https://github.com/mogenius/homebrew-mocli"
+  url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.0.0/mocli-1.0.0-darwin-arm64"
   sha256 "950dce2b0a5180a8683de2a1c149bad8627657f01282c632ab5cb845aa0fc236"
   license "MIT License"
   version "1.0.0"
