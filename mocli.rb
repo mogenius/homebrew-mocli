8f80d75e9047d73ff46285cd6951c98eea839fce
mogenius-cli <mogenius-cli@mogenius.com>
2023-08-17 20:57:34 +0000

    v1.2.0

--- mocli.rb
+++ mocli.rb
@@ -4,62 +4,62 @@ class Mocli < Formula
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.1.6/mocli-1.1.6-darwin-arm64.tar.gz"
-      sha256 "f56031f3a477f0a3cd38d4f29c7738a0a497914c63c31b1fcefbe57b7b126e1c"
+      url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.0/mocli-1.2.0-darwin-arm64.tar.gz"
+      sha256 "7398eb6bfdd3458d150461ac42697fb975a2654165f90a94ab15170def03d81c"
     elsif Hardware::CPU.intel?
-      url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.1.6/mocli-1.1.6-darwin-amd64.tar.gz"
-      sha256 "bda0d04111451f14f686dee98561ce45774df3abdf0288586a59b219502c7f86"
+      url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.0/mocli-1.2.0-darwin-amd64.tar.gz"
+      sha256 "e893ce3d093c4a9eb65ad0177fc9342143e2e74e25d2f5ed11e6ff3d3aad9a18"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.1.6/mocli-1.1.6-linux-amd64.tar.gz"
-        sha256 "f75d2d5b7164505eb2b515f39025374e0d42a31bf7e41622d6041cc92014103e"
+        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.0/mocli-1.2.0-linux-amd64.tar.gz"
+        sha256 "ecd562417f99f2b3bd5b1a94e3ddf55e3482a41a04c32ac5f2072f8e71ef7e20"
       else
-        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.1.6/mocli-1.1.6-linux-386.tar.gz"
-        sha256 "642892dd21d6a0066f1b7a13d44e5b222bf8d233d83808e7ffc2cbc07ba04405"
+        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.0/mocli-1.2.0-linux-386.tar.gz"
+        sha256 "191d9a4344d32ace54f449ad9adafdc0c8a77ac18cb7fcfa1c93f2e65811972f"
       end
     elif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.1.6/mocli-1.1.6-linux-arm64.tar.gz"
-        sha256 "3d7de379c85a98c7e211edca8d00310b64eac8d0c8499de14dd24648d25b765f"
+        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.0/mocli-1.2.0-linux-arm64.tar.gz"
+        sha256 "cb4e1193059d4ec2ea1004993764088e937fd2e9d0352604831dac59a524e4fc"
       else
-        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.1.6/mocli-1.1.6-linux-arm.tar.gz"
-        sha256 "7368d4ecddd8b150f2d294165789941d215b5c3d27a857521d9e653217d23fab"
+        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.0/mocli-1.2.0-linux-arm.tar.gz"
+        sha256 "893d0be8432745060d05d778fe4e8d61b363619e536589ea535e68f87639cf47"
       end
     end
   end
   
-  version "1.1.6"
+  version "1.2.0"
   license "MIT"
 
   def install
   if OS.mac?
     if Hardware::CPU.arm?
       # Installation steps for macOS ARM64
-      bin.install "mocli-1.1.6-darwin-arm64" => "mocli"
+      bin.install "mocli-1.2.0-darwin-arm64" => "mocli"
     elsif Hardware::CPU.intel?
       # Installation steps for macOS AMD64
-      bin.install "mocli-1.1.6-darwin-amd64" => "mocli"
+      bin.install "mocli-1.2.0-darwin-amd64" => "mocli"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux AMD64
-        bin.install "mocli-1.1.6-linux-amd64" => "mocli"
+        bin.install "mocli-1.2.0-linux-amd64" => "mocli"
       else
         # Installation steps for Linux 386
-        bin.install "mocli-1.1.6-linux-386" => "mocli"
+        bin.install "mocli-1.2.0-linux-386" => "mocli"
       end
     elsif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux ARM64
-        bin.install "mocli-1.1.6-linux-arm64" => "mocli"
+        bin.install "mocli-1.2.0-linux-arm64" => "mocli"
       else
         # Installation steps for Linux ARM
-        bin.install "mocli-1.1.6-linux-arm" => "mocli"
+        bin.install "mocli-1.2.0-linux-arm" => "mocli"
       end
     end
   end
