6c70b6f3582a8bb15b3801f5fda3f7881a8a2860
mocli <mocli@mogenius.com>
2023-09-01 09:42:20 +0000

    v1.2.13

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+
+### [1.2.13](https://github.com/mogenius/mocli/compare/v1.2.12...v1.2.13) (2023-09-01)
+
+
+### Bug Fixes
+
+* pipeline fix. ([e9a9533](https://github.com/mogenius/mocli/commit/e9a953381a3f650fd07268c50c96361cf2951755))
+
+### [1.2.12](https://github.com/mogenius/mocli/compare/v1.2.11...v1.2.12) (2023-09-01)
+
+
+### Bug Fixes
+
+* minor fix. ([4f00537](https://github.com/mogenius/mocli/commit/4f005375f6b93c1d193a0077bb4bfa2e49c83acf))
+
+### [1.2.11](https://github.com/mogenius/mocli/compare/v1.2.10...v1.2.11) (2023-09-01)
+
+
+### Bug Fixes
+
+* minor fix. ([064ee09](https://github.com/mogenius/mocli/commit/064ee09da5bacd348c3982d3700943cbb9049a13))
+
+### [1.2.10](https://github.com/mogenius/mocli/compare/v1.2.9...v1.2.10) (2023-09-01)
+
+
+### Bug Fixes
+
+* added automated scoop support. Switched to automated semantic release. Renamed module to mocli. ([871e569](https://github.com/mogenius/mocli/commit/871e569e170e327b2b257bb16725bab5165f8f9f))
+* minor fix. ([35007a2](https://github.com/mogenius/mocli/commit/35007a244e1aa9125dd7e7a4b7895156c59d4c76))
+* Switched to fmt statements. ([6cdf3d9](https://github.com/mogenius/mocli/commit/6cdf3d962c8e46bd69c348eb5a2eaac6a45cb889))
--- mocli.rb
+++ mocli.rb
@@ -4,62 +4,62 @@ class Mocli < Formula
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.9/mocli-1.2.9-darwin-arm64.tar.gz"
-      sha256 "b52165e74f063236bc5b8a6a12be9bd5753de6d2231c87b8b124fb63c589f011"
+      url "https://github.com/mogenius/homebrew-mocli/releases/download/vv1.2.13/mocli-v1.2.13-darwin-arm64.tar.gz"
+      sha256 "c20d58a97f92d9ceac8bef6fbb61e10ca40881e45efe903163877a93b47851d4"
     elsif Hardware::CPU.intel?
-      url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.9/mocli-1.2.9-darwin-amd64.tar.gz"
-      sha256 "cc04ae9c0dfe54097cce82d468cfb6bc30e5de25768470f24599d0e498ea7eff"
+      url "https://github.com/mogenius/homebrew-mocli/releases/download/vv1.2.13/mocli-v1.2.13-darwin-amd64.tar.gz"
+      sha256 "ce22fded3ae3a70fb4ee9e60583ab53612a5fa677af11ed9b16ed1c0ab2a860e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.9/mocli-1.2.9-linux-amd64.tar.gz"
-        sha256 "76523e767a5965b15c860d38fb429c6bc188d3b81b4f95fc57475a9159701365"
+        url "https://github.com/mogenius/homebrew-mocli/releases/download/vv1.2.13/mocli-v1.2.13-linux-amd64.tar.gz"
+        sha256 "a7dab3d704779ba9aa862f3976c118adaba02dc88bf5ca26c67e72a999783eb7"
       else
-        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.9/mocli-1.2.9-linux-386.tar.gz"
-        sha256 "09866a598fd87ccb48db6e56659258cadc56d02855f67adeaf46cf5e60b22f03"
+        url "https://github.com/mogenius/homebrew-mocli/releases/download/vv1.2.13/mocli-v1.2.13-linux-386.tar.gz"
+        sha256 "8b1ea913cefe57621ceb002918d12b65861fb6baf1bb11341c1f91854ef8a4a7"
       end
     elif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.9/mocli-1.2.9-linux-arm64.tar.gz"
-        sha256 "1a0d875e864da6a6fd03084ecdc53ef8d140b0cac6d17651a7dd280524564b53"
+        url "https://github.com/mogenius/homebrew-mocli/releases/download/vv1.2.13/mocli-v1.2.13-linux-arm64.tar.gz"
+        sha256 "9419ec15596e4a122c9a552f5d4ca58a3a0051fdac6faeba15bea3e990b6c52b"
       else
-        url "https://github.com/mogenius/homebrew-mocli/releases/download/v1.2.9/mocli-1.2.9-linux-arm.tar.gz"
-        sha256 "f53ce3790f0d05f58cceb32b7cea69272e6b716020b16612820e784b6e687079"
+        url "https://github.com/mogenius/homebrew-mocli/releases/download/vv1.2.13/mocli-v1.2.13-linux-arm.tar.gz"
+        sha256 "50b4801c0cf054c93883ec9fa2a4229edf21023a48b6e34cfdf62b76d1ea4d49"
       end
     end
   end
   
-  version "1.2.9"
+  version "v1.2.13"
   license "MIT"
 
   def install
   if OS.mac?
     if Hardware::CPU.arm?
       # Installation steps for macOS ARM64
-      bin.install "mocli-1.2.9-darwin-arm64" => "mocli"
+      bin.install "mocli-v1.2.13-darwin-arm64" => "mocli"
     elsif Hardware::CPU.intel?
       # Installation steps for macOS AMD64
-      bin.install "mocli-1.2.9-darwin-amd64" => "mocli"
+      bin.install "mocli-v1.2.13-darwin-amd64" => "mocli"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux AMD64
-        bin.install "mocli-1.2.9-linux-amd64" => "mocli"
+        bin.install "mocli-v1.2.13-linux-amd64" => "mocli"
       else
         # Installation steps for Linux 386
-        bin.install "mocli-1.2.9-linux-386" => "mocli"
+        bin.install "mocli-v1.2.13-linux-386" => "mocli"
       end
     elsif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux ARM64
-        bin.install "mocli-1.2.9-linux-arm64" => "mocli"
+        bin.install "mocli-v1.2.13-linux-arm64" => "mocli"
       else
         # Installation steps for Linux ARM
-        bin.install "mocli-1.2.9-linux-arm" => "mocli"
+        bin.install "mocli-v1.2.13-linux-arm" => "mocli"
       end
     end
   end
